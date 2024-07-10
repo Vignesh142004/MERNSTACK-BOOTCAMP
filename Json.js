11c703d9125a3e71a2743ce4241f85ab2a7fb98c
Vignesh142004 <vigneshranganathan14@gmail.com>
2024-07-10 17:06:30 +0530

    fullstack

---
+++ Json.js
@@ -0,0 +1,6 @@
+//Creation of json object
+const person={name:"vignesh"};//direct object.
+const person1={}; //empty object
+const person2=new Object; //using object method.
+const person3=Object.create(null);
+console.log(person);
